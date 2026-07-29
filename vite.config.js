878cd0fc28a6637e3b0910d762229217360d8636
Mukul Mittal <mittalmukul1197@gmail.com>
2026-07-29 13:20:22 +0530

    readme.md

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Vibe-To-Viable/',
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   server: {
     host: true,
     port: 3000,
